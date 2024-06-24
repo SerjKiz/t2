d8649fc45be4a2f88a675498f0f55bf741f90934
SerjKiz <skizlasov@inno.tech>
2024-06-24 14:37:02 +0300

    Add files via upload

---
+++ src/inno/CachingHandLer.java
@@ -0,0 +1,48 @@
+package inno;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+class CachingHandLer implements InvocationHandler {
+
+
+    private Object objectIncome;
+
+    private Map<Method, Object> cacheObj = new HashMap<>();
+
+
+
+
+    public CachingHandLer(Object objectIncome) {
+
+        this.objectIncome = objectIncome;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        Method methodClass =this.objectIncome.getClass().getMethod(method.getName(), method.getParameterTypes());
+
+
+
+
+        if (methodClass.isAnnotationPresent(Mutator.class)){
+            cacheObj.clear();
+        }
+
+        if (methodClass.isAnnotationPresent(Cache.class)){
+            if (cacheObj.containsKey(methodClass)){
+                return cacheObj.get(methodClass);
+            }
+        }
+
+
+        Object res = method.invoke(this.objectIncome, args);
+        if (methodClass.isAnnotationPresent(Cache.class)){
+            cacheObj.put(methodClass,res);
+        }
+        return res;
+    }
+}
---
+++ src/inno/Mutator.java
@@ -0,0 +1,8 @@
+package inno;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Mutator {
+}
